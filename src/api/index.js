30bfea348d001e1dde4f2a1fc8a4b1b131d17c57
Songguohao <947662512@qq.com>
2021-06-21 21:20:34 +0800

    新增歌手

--- src/api/index.js
+++ src/api/index.js
@@ -2,3 +2,6 @@ import { post } from './http'
 
 // 判断管理员是否登陆成功
 export const getLoginStatus = (params) => post(`admin/login/status`, params)
+
+// 添加歌手
+export const setSinger = (params) => post(`singer/add`, params)
